05cdc238fd3920557796871ea53c34119e322b74
Maria Maltseva <mariamaltseva1106@gmail.com>
2024-10-18 10:43:55 +0300

    tests, readme

--- src/category.py
+++ src/category.py
@@ -10,6 +10,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        """Метод, принимающий на вход продукт product, добавляющий его в категорию и увеличивающий счетчик продуктов
+        product_count на единицу"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер, возвращающий строку со всеми продуктами"""
+        product_str = ''
+        for product in self.__products:
+            product_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+        return product_str
--- src/product.py
+++ src/product.py
@@ -1,6 +1,6 @@
-
 class Product:
     """Класс продукта"""
+
     name: str
     description: str
     price: float
@@ -9,5 +9,24 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product):
+        """Класс-метод принимает на вход cls и продукт для создания в виде словаря. Возвращает экземпляр класса
+        Product"""
+        return cls(**product)
+
+    @property
+    def price(self):
+        """Геттер, возвращающий значение приватного атрибута __price"""
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """Сеттер, изменяющий значение приватного атрибута __price"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
